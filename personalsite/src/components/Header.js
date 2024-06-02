7bf1966ae0b9985f767637576121b63d8382f502
Shakib Ahmed <shakibahmed@Shakibs-MacBook-Air.local>
2024-06-01 23:39:52 -0700

    logo and escape key

--- personalsite/src/components/Header.js
+++ personalsite/src/components/Header.js
@@ -2,18 +2,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.svg'; 
+import personallogo from '../assets/personallogo.jpeg'
 
 const Header = () => {
   return (
     <header className="header">
-        <div className="logo-container">
-            <img src={logo} alt="Logo" className="logo" />
-        </div>
-        <nav className="nav">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/projects" className="nav-link">Projects</Link>
-            <Link to="/resume" className="nav-link">Resume</Link>
-        </nav>
+      <div className="logo-container">
+        <Link to="/">
+          <img src={personallogo} alt="Logo" className="logo" />
+        </Link>
+      </div>
+      <nav className="nav">
+        <Link to="/projects" className="nav-link">Projects</Link>
+        <Link to="/resume" className="nav-link">Resume</Link>
+      </nav>
     </header>
   );
 };
--- personalsite/src/components/Modal.js
+++ personalsite/src/components/Modal.js
@@ -1,8 +1,12 @@
 // Modal.js
 import React from 'react';
 import "../styles/ModalStyles.css";
+import useEscapeKey from './UseEscapeKey';
 
 const Modal = ({ onClose, children, isActive }) => {
+    useEscapeKey(onClose)
+
+    if (!isActive) return null;
     return (
         <div className={`modal-overlay ${isActive ? 'active' : ''}`} onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
