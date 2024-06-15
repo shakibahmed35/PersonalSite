def8a7f882cb39a165511638b3913b4339ebe567
Shakib Ahmed <shakibahmed@Shakibs-MacBook-Air.local>
2024-06-14 22:58:37 -0700

    fixing loading

--- personalsite/src/App.js
+++ personalsite/src/App.js
@@ -2,7 +2,10 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Main from './layouts/Main'
 import './static/css/main.scss'
+
+const { PUBLIC_URL } = process.env;
 
 const Projects = lazy(() => import('./pages/Projects'));
 const Resume = lazy(() => import('./pages/Resume'));
@@ -13,15 +16,17 @@ const About = lazy(() => import('./pages/About'));
 
 const App = () => {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Index />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/resume' element={<Resume />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/about' element={<About />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
+      <BrowserRouter basename={PUBLIC_URL}>
+        <Suspense fallback={<Main />}>
+          <Routes>
+            <Route path='/' element={<Index />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/resume' element={<Resume />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+          </Suspense>
       </BrowserRouter>
   );
 };
