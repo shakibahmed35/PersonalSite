306e88ecb254e69c95f6f379e141040ab4ca7025
Shakib Ahmed <shakibahmed@Shakibs-MacBook-Air.local>
2024-06-15 21:55:40 -0700

    fixing index.js

--- personalsite/src/pages/Index.js
+++ personalsite/src/pages/Index.js
@@ -6,7 +6,7 @@ import Main from '../layouts/Main'
 const Index = () => {
     return(
         <Main
-            description={"Shakib's website. ASU Graduate. Sports Legend. Software Developer"}
+            description={"Shakib's website. ASU Graduate. Basketball Legend. Software Developer"}
         >
             <article className='post' id='index'>
                 <header>
@@ -18,10 +18,11 @@ const Index = () => {
                     </div>
                 </header>
                 <p>
-                    Welcome to my wesbite. Please feel free to read more <Link to='/about'>About me</Link>,
-                    or you can check out some other pages: {' '}
+                    Welcome to my website. This was built out of React and hosted on GitHub Pages. 
+                    Please feel free to read more <Link to='/about'>About me</Link>.
+                    Also, please do check out my, {' '}
                     <Link to='/resume'>Resume</Link>, {' '} <Link to='/projects'>Projects</Link>, {' '} 
-                    <Link to='/contact'>Contact</Link>
+                    <Link to='/contact'>Contact</Link> Information.
                 </p>
             </article>
         </Main>
